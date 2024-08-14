db0bf77d25c9b28933874ab48cc55f4cc0601924
mari <mariroman604@gmail.com>
2024-08-14 16:21:28 +0200

    Fix JSON parsing issue

--- firebase/admin.js
+++ firebase/admin.js
@@ -4,9 +4,16 @@ import { config } from 'dotenv';
 config();
 
 console.log('FIREBASE_CONFIG:', process.env.FIREBASE_CONFIG);
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
-console.log('PARSE:', firebaseConfig);
+let firebaseConfig;
+
+try {
+    const serviceAccountStr = process.env.FIREBASE_CONFIG.replace(/\\n/g, '\n');
+    firebaseConfig = JSON.parse(serviceAccountStr);
+    console.log('Service Account:', firebaseConfig);
+} catch (error) {
+    console.error('Error parsing JSON:', error);
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(firebaseConfig),
