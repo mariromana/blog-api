21f4954012b0fbfb353e200e23cea8f37e1e40f4
mari <mariroman604@gmail.com>
2024-08-14 15:54:26 +0200

    add debug logging for Firebase

--- firebase/admin.js
+++ firebase/admin.js
@@ -3,7 +3,10 @@ import { config } from 'dotenv';
 
 config();
 
+console.log('FIREBASE_CONFIG:', process.env.FIREBASE_CONFIG);
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+
+console.log('PARSE:', firebaseConfig);
 
 admin.initializeApp({
     credential: admin.credential.cert(firebaseConfig),
