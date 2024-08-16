37caaa6ce262147fbff7ddcd884cd1835e69696e
mari <mariroman604@gmail.com>
2024-08-16 17:48:16 +0200

    change firebase key

--- firebase/admin.js
+++ firebase/admin.js
@@ -3,16 +3,11 @@ import { config } from 'dotenv';
 
 config();
 
-const decodedPrivateKey = Buffer.from(
-    process.env.FIREBASE_PRIVATE_KEY,
-    'base64'
-).toString('utf-8');
-
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: decodedPrivateKey.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
