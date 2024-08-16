dcc233fa71fec4fed7bce79a8697caf5ede78d55
mari <mariroman604@gmail.com>
2024-08-16 17:19:58 +0200

    Add base64-encoded Firebase private key

--- firebase/admin.js
+++ firebase/admin.js
@@ -3,11 +3,16 @@ import { config } from 'dotenv';
 
 config();
 
+const decodedPrivateKey = Buffer.from(
+    process.env.FIREBASE_PRIVATE_KEY,
+    'base64'
+).toString('utf-8');
+
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        privateKey: decodedPrivateKey.replace(/\\n/g, '\n'),
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
